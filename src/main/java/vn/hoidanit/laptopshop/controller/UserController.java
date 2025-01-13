4a6b6ab6f2b32c1afc7b7eeed9e02d4081236d0d
GiangHV <giang4451050083@st.qnu.edu.vn>
2025-01-13 15:25:54 +0700

    Submit Form Section

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,24 +1,19 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -27,8 +22,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
-    public String getHomePage() {
-        return userService.handleHello();
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String text = this.userService.handleHello();
+        model.addAttribute("vien", text);
+        model.addAttribute("title", "mai phuong");
+        return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User request) {
+        System.out.println(request);
+
+        return "hello";
+    }
+
 }
