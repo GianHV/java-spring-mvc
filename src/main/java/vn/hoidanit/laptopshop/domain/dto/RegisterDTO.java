57b975678618c25c184c0ed4dfc305319ef6df60
GiangHV <giang4451050083@st.qnu.edu.vn>
2025-01-16 19:34:36 +0700

    done filter

--- src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
@@ -1,7 +1,6 @@
 package vn.hoidanit.laptopshop.domain.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import vn.hoidanit.laptopshop.service.validator.RegisterChecked;
 
