7ad2042da37e19fea36a93a69a60c421bfc565ed
GiangHV <giang4451050083@st.qnu.edu.vn>
2025-01-16 13:06:08 +0700

    done design cart detail

--- src/main/java/vn/hoidanit/laptopshop/config/CustomSuccessHandler.java
+++ src/main/java/vn/hoidanit/laptopshop/config/CustomSuccessHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -17,25 +18,57 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import vn.hoidanit.laptopshop.domain.Cart;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
-/*
- * This class is a extension of spring security, allow user
- * to customize flow when user login successfully
- */
 public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 
     @Autowired
     private UserService userService;
 
-    // instance to redirect url
+    protected String determineTargetUrl(final Authentication authentication) {
+
+        Map<String, String> roleTargetUrlMap = new HashMap<>();
+        roleTargetUrlMap.put("ROLE_USER", "/");
+        roleTargetUrlMap.put("ROLE_ADMIN", "/admin");
+
+        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (final GrantedAuthority grantedAuthority : authorities) {
+            String authorityName = grantedAuthority.getAuthority();
+            if (roleTargetUrlMap.containsKey(authorityName)) {
+                return roleTargetUrlMap.get(authorityName);
+            }
+        }
+
+        throw new IllegalStateException();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, Authentication authentication) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        // get email
+        String email = authentication.getName();
+        // query user
+        User user = this.userService.getUserByEmail(email);
+        if (user != null) {
+            session.setAttribute("fullName", user.getFullName());
+            session.setAttribute("avatar", user.getAvatar());
+            session.setAttribute("id", user.getId());
+            session.setAttribute("email", user.getEmail());
+            int sum = Optional.ofNullable(user.getCart())
+                    .map(Cart::getSum)
+                    .orElse(0);
+            session.setAttribute("sum", sum);
+        }
+
+    }
+
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
-    /*
-     * When user login successfully, this function will be execute
-     * it will redirect
-     */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
@@ -52,46 +85,4 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 
     }
 
-    /*
-     * specific url will redirect by url of user
-     * when user login successfully
-     */
-    protected String determineTargetUrl(final Authentication authentication) {
-
-        // if user is admin navigate to admin page
-        // else redirect to homepage
-        Map<String, String> roleTargetUrlMap = new HashMap<>();
-        roleTargetUrlMap.put("ROLE_USER", "/");
-        roleTargetUrlMap.put("ROLE_ADMIN", "/admin");
-
-        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        for (final GrantedAuthority grantedAuthority : authorities) {
-            String authorityName = grantedAuthority.getAuthority();
-            if (roleTargetUrlMap.containsKey(authorityName)) {
-                return roleTargetUrlMap.get(authorityName);
-            }
-        }
-
-        throw new IllegalStateException();
-    }
-
-    /*
-     * clear session before
-     */
-    protected void clearAuthenticationAttributes(HttpServletRequest request, Authentication authentication) {
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            return;
-        }
-        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-        // get email
-        String email = authentication.getName();
-        // query user
-        User user = this.userService.getUserByEmail(email);
-        if (user != null) {
-            session.setAttribute("fullName", user.getFullName());
-            session.setAttribute("avatar", user.getAvatar());
-        }
-
-    }
 }
